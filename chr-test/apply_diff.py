695570a9388f376186ac6a39bcba6b293949c6bf
Andy Lianghua Xu <lx2180@columbia.edu>
2017-12-16 21:49:22 -0500

    completed delta application code

--- chr-test/apply_diff.py
+++ chr-test/apply_diff.py
@@ -1,27 +1,18 @@
+#!/usr/bin/env python3
+
 from Bio import SeqIO,Seq,SeqRecord
 from Bio.Alphabet import DNAAlphabet
+import sys
 
 def readSequence(name):
     seqs = SeqIO.parse(name, 'fasta')
     seq = ""
     for i, record in enumerate(seqs):
         assert(i < 1) # make suree there's only one sequence per file
-        print(record)
         seq = record.seq
     return seq
 
-def checkIntegrity(seq, diffFileName, checkNew=False):
-    with open (diffFileName, "r") as f:
-        for line in f:
-            delta = line.split(',')
-            if delta[0] == "0":
-                posOfDelta=int(delta[2])
-                if checkNew:
-                    assert(delta[3][2] == seq[posOfDelta-1]) # make sure we are replacing the right thing
-                else:
-                    assert(delta[3][0] == seq[posOfDelta-1]) # make sure we are replacing the right thing
-
-def serializeDiffs(diffFileName):
+def serializeDiffs(diffFileName, chrName):
     diffs = []
     lastDelPos = 0
     lastDelLength = 0
@@ -29,11 +20,15 @@ def serializeDiffs(diffFileName):
         for line in f:
             delta = line.split(',')
             posOfDelta = int(delta[2])
+            if delta[1] != chrName:
+                continue
             if delta[0] == "0": #SNP
                 diffs.append((0, posOfDelta - 1, delta[3][2]))
             elif delta[0] == "1": # deletion
                 lengthToRemove = len(delta[3].split("/")[0])
                 contentToRemove = delta[3].split("/")[0]
+                if (posOfDelta - 1 - lastDelPos < lastDelLength):
+                    continue
                 diffs.append((1, posOfDelta - 1, lengthToRemove, contentToRemove))
                 lastDelPos = posOfDelta - 1
                 lastDelLength = lengthToRemove
@@ -43,9 +38,8 @@ def serializeDiffs(diffFileName):
                 diffs.append((2, posOfDelta - 1, stringToInsert))
     diffs.sort(key=lambda x:x[1])
     return diffs
-def applyDiff(seq, diffs):
-    totalInsertion = 0
-    totalDeletion = 0
+
+def applyDiffOn(seq, diffs):
     newSeqList = []
     nextPosToPush = 0
     for diff in diffs:
@@ -53,36 +47,45 @@ def applyDiff(seq, diffs):
         posOfDelta=int(diff[1])
         contentOfDelta = diff[2]
         if deltaType == 0:
-
             newSeqList.append(str(seq[nextPosToPush:posOfDelta]))
             newSeqList.append(contentOfDelta) # the new SNP
             nextPosToPush = posOfDelta + 1
         elif deltaType == 2: # insertion
-            continue
             newSeqList.append(str(seq[nextPosToPush:posOfDelta + 1]))
             newSeqList.append(contentOfDelta) # the new sequence
             nextPosToPush = posOfDelta + 1
-            totalInsertion += len(contentOfDelta)
         else: # deletion
             assert(deltaType == 1)
-            newSeqList.append(str(seq[nextPosToPush:posOfDelta + 1]))
-            nextPosToPush = posOfDelta + contentOfDelta + 1
-            # assert(skipped == diff[3])
-            totalDeletion += contentOfDelta
+            newSeqList.append(str(seq[nextPosToPush:posOfDelta]))
+            assert(diff[3] == seq[posOfDelta: posOfDelta + contentOfDelta])# make sure we are deleting the same thing
+            nextPosToPush = posOfDelta + contentOfDelta
     newSeqList.append(str(seq[nextPosToPush:]))
     newSeq = "".join(newSeqList)
-    print("inserted: {}, removed: {}".format(totalInsertion, totalDeletion))
+    print("Total {} deltas applied.".format(len(diffs)))
     return newSeq
 
-def main():
+def run(chrName, refDirectory, deltaFile):
     # seq = readSequence('chrY-small.fa')
-    seq = readSequence('chrY.fa')
-    print("Read sequence of length: {}".format(len(seq)))
-    # checkIntegrity(seq, "chrY-JWB-unified-file.txt")
-    diffs = serializeDiffs("./chrY-JWB-unified-file.txt")
+    seq = readSequence('{}/{}.fa'.format(refDirectory, chrName))
+    print("Read reference sequence of length: {}".format(len(seq)))
+    diffs = serializeDiffs(deltaFile, chrName)
     # diffs = serializeDiffs("smalldel.txt")
-    newSeq = applyDiff(seq, diffs)
-    print("newSeq length: {}".format(len(newSeq)))
-    print("change: {}".format(len(newSeq) - len(seq)))
-    # checkIntegrity(newSeq, "chrY-JWB-unified-file.txt", checkNew=True)
+    print("Found {} deltas".format(len(diffs)))
+    newSeq = applyDiffOn(seq, diffs)
+    print("Constructed new sequence length: {}".format(len(newSeq)))
+    print("Diff between new and reference: {}".format(len(newSeq) - len(seq)))
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: ./apply_diff.py <chrX ref directory> <delta file>")
+        sys.exit()
+    chrs = [str(i) for i in range(1, 23)]
+    chrs += ["X", "Y", "M"]
+    for chrNum in chrs:
+        name = "chr{}".format(chrNum)
+        print("==BEGIN {}===".format(name))
+        run(name, sys.argv[1], sys.argv[2])
+        print("==END {}===".format(name))
+        print("")
 main()
